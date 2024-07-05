949be8626caaba6b32eb5583ab1182b3628c0679
kondareddy007 <kondareddy.devops@gmail.com>
2024-07-05 12:26:04 +0530

    midified variable

---
+++ sellscript/02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh : Hi sures, How are you"
+echo "Suresh : hi Suresh, I am fine"
+echo " Ramesh : Suresh i am learning devops"
+echo " Suresh : ramesh i am also learning devops"
