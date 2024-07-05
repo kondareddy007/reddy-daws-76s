a85b3a906dea53451601b2ce57f4c41df440345b
kondareddy007 <kondareddy.devops@gmail.com>
2024-07-05 14:50:54 +0530

    add variable in script

--- sellscript/2-variable.sh
+++ sellscript/2-variable.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "Ramesh : Hi sures, Good morning"
-echo "Suresh : Hi Ramesh, Good morning"
-echo "Ramesh : Hi sures, How are you"
-echo "Suresh : Hi Ramesh, I am fine"
+#add external variables
+VAR1=Ramesh
+VAR2=Suresh
+
+echo "$VAR1 : Hi $VAR2, Good morning"
+echo "$VAR2 : Hi $VAR1, Good morning"
+echo "$VAR1 : Hi $VAR2, How are you"
+echo "$VAR2 : Hi $VAR1, I am fine"
