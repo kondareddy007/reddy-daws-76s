a795ae1eb329629419af9f931e2860e0f1e67602
kondareddy007 <kondareddy.devops@gmail.com>
2024-07-12 15:48:40 +0530

    added

--- sellscript/16-14-installed_packages.sh
+++ sellscript/16-14-installed_packages.sh
@@ -21,6 +21,7 @@ VALIDATE(){
 if [ $ID -ne 0 ]
 then
     echo "Error:: Please run this script with root access"
+    exit 1
 else
     echo "you are root user"
 fi
@@ -35,4 +36,6 @@ do
     else  
         echo "$package already installed ...SKIPPING"
     fi
+done
+
         
