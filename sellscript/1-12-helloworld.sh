8280a839fea16cff8e4883d5695aae6dea14473c
kondareddy007 <kondareddy.devops@gmail.com>
2024-07-12 10:38:48 +0530

    added

--- sellscript/1-12-helloworld.sh
+++ sellscript/1-12-helloworld.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "hello world"
+a=$1
+b=$2
+add=$((a+b))
+echo "result is :$add"
---
+++ sellscript/14-13-logs.sh
@@ -0,0 +1,30 @@
+### This scipt is used for storing logs ###
+
+#!/bin/bash
+ID=$(id -u)
+echo "display the script name:: $0"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+then 
+    echo "ERROR::$2 ... Failed"
+    exit 1
+else
+    echo "$2 ..... Success"
+fi
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo "Error:: Please run this script with root user"
+    exit 1 #You can give otherthan 0.
+else 
+    echo "you are root user"  
+fi
+
+yum install mysql -y 
+
+VALIDATE $? "Installing MYSQL"
+
+yum install git -y 
+
+VALIDATE $? "Installing GIT"
