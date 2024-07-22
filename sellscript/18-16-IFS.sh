698ce2b357658e5df28e06034ecae670dfd4f253
kondareddy007 <kondareddy.devops@gmail.com>
2024-07-22 10:00:52 +0530

    roboshop

--- sellscript/18-16-IFS.sh
+++ sellscript/18-16-IFS.sh
@@ -9,8 +9,10 @@ then
    echo -s "$R Surce file: $FILE does not exists $N"
 fi
 
-while IFS=":" read -r username
+while IFS=":" read -r username password user_id
 do
-  echo "$username"
+  echo "User_name is: $username"
+  echo "Password is : $password"
+  echo " User_id is : $user_id"
 
 done < $FILE
